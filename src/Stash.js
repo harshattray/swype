937b4f9959c3ff74b00c4a8b9a45b5255377eca5
Harsha Attray <harsh.attray@gmail.com>
2017-05-06 23:17:04 +0530

    feat: draggable stash enabled

--- src/Stash.js
+++ src/Stash.js
@@ -24,17 +24,25 @@ class Stash extends Component {
     this.state = { panResponder, position };
   }
   renderCards() {
-    return this.props.data.map(item => {
-      return this.props.renderCard(item);
+    return this.props.data.map((item,index) => {
+      if (index === 0){
+        return (
+          <Animated.View key={item.id}
+            style={this.state.position.getLayout()}
+            {...this.state.panResponder.panHandlers}
+            >
+            {this.props.renderCard(item)}
+          </Animated.View>
+        );
+      }
+      return this.props.renderCard(item)
     });
   }
   render() {
     return (
-     <Animated.View
-       style={this.state.position.getLayout()}
-       {...this.state.panResponder.panHandlers}>
+     <View>
        {this.renderCards()}
-     </Animated.View>
+     </View>
     );
   }
 }
