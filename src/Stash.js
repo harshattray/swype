892c3e8f157e1831c8d3aaa1e69992774bf3da81
Harsha Attray <harsh.attray@gmail.com>
2017-05-06 23:00:32 +0530

    feat: draggable stash enabled

--- src/Stash.js
+++ src/Stash.js
@@ -8,15 +8,20 @@ import {
 class Stash extends Component {
   constructor(props) {
     super(props);
+    const position = new Animated.ValueXY();
     const panResponder = PanResponder.create({
       onStartShouldSetPanResponder: () => true,
       onPanResponderMove: (event, gesture) => { //info abt user actions ->gesture
         console.log(gesture);
+        position.setValue({
+          x: gesture.dx,
+          y: gesture.dy,
+        })
       },
       onPanResponderRelease: () => {}
 
     });
-    this.state = { panResponder };
+    this.state = { panResponder, position };
   }
   renderCards() {
     return this.props.data.map(item => {
@@ -25,9 +30,11 @@ class Stash extends Component {
   }
   render() {
     return (
-     <View {...this.state.panResponder.panHandlers}>
+     <Animated.View
+       style={this.state.position.getLayout()}
+       {...this.state.panResponder.panHandlers}>
        {this.renderCards()}
-     </View>
+     </Animated.View>
     );
   }
 }
