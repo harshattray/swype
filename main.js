09589d337dbc8eeaf4712931da0c9c99727f5fdb
Harsha Attray <harsh.attray@gmail.com>
2017-05-06 16:20:50 +0530

    Feat: Stash component and render props defined

--- main.js
+++ main.js
@@ -2,11 +2,31 @@ import Expo from 'expo';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+import Stash from './src/Stash';
+
+const DATA = [
+  { id: 1, text: 'Card #1', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg' },
+  { id: 2, text: 'Card #2', uri: 'http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg' },
+  { id: 3, text: 'Card #3', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg' },
+  { id: 4, text: 'Card #4', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg' },
+  { id: 5, text: 'Card #5', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg' },
+  { id: 6, text: 'Card #6', uri: 'http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg' },
+  { id: 7, text: 'Card #7', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg' },
+  { id: 8, text: 'Card #8', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg' },
+];
+
 class App extends React.Component {
+   renderCard(item) {
+     return (
+       <Text>{item.text}</Text>
+     );
+   }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up main.js to start working on your app!</Text>
+        <Stash data={DATA} renderCard={this.renderCard} />
       </View>
     );
   }
@@ -16,8 +36,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
 
---
+++ src/Stash.js
@@ -0,0 +1,12 @@
+import React , { Component } from 'react';
+import { View, Animated } from 'react-native';
+
+class Stash extends Componenet {
+  render(){
+    return(
+     <View />
+    )
+  }
+}
+
+export default Stash;
