c842651c368580e2a21e2ed9bcd9774d2e597776
Harsha Attray <harsh.attray@gmail.com>
2017-06-08 16:17:37 +0530

    feat: card ui changes

--- main.js
+++ main.js
@@ -5,10 +5,10 @@ import { Card, Button } from 'react-native-elements';
 import Stash from './src/Stash';
 
 const DATA = [
-  { id: 1, text: 'Card #1', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg' },
-  { id: 2, text: 'Card #2', uri: 'http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg' },
-  { id: 3, text: 'Card #3', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg' },
-  { id: 4, text: 'Card #4', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg' },
+  { id: 1, text: 'Card #1', uri: 'https://cdn.pixabay.com/photo/2017/05/31/18/38/sea-2361247_960_720.jpg' },
+  { id: 2, text: 'Card #2', uri: 'https://cdn.pixabay.com/photo/2014/02/12/16/34/sky-264778_960_720.jpg' },
+  { id: 3, text: 'Card #3', uri: 'https://cdn.pixabay.com/photo/2014/10/15/06/09/lake-489289_960_720.jpg' },
+  { id: 4, text: 'Card #4', uri: 'https://cdn.pixabay.com/photo/2016/08/13/18/34/lake-tahoe-1591339_960_720.jpg' },
   { id: 5, text: 'Card #5', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg' },
   { id: 6, text: 'Card #6', uri: 'http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg' },
   { id: 7, text: 'Card #7', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg' },
@@ -23,7 +23,7 @@ class App extends React.Component {
          image={{ uri: item.uri }}
        >
        <Text style={{ marginBottom: 10 }}>Feed comes here</Text>
-       <Button icon={{ name: 'cloud' }} backgroundColor='#03A9F4' title="View Now" />
+       <Button icon={{ name: 'rowing' }} backgroundColor='#ffa419' title="Explore" />
        </Card>
      );
    }
